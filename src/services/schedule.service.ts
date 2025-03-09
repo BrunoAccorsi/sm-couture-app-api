b5d57ba5d032110f7353ed47a037c3f13bc0fb17
Bruno Accorsi Bergoli <b.bergoli@gmail.com>
2025-03-08 22:43:32 -0700

    feat(schedule): add ordering to upcoming schedules by status and start time

--- src/services/schedule.service.ts
+++ src/services/schedule.service.ts
@@ -2,7 +2,7 @@ import { db } from '@/db';
 import { userSchedule } from '@/db/schema';
 import { SelectUserSChedule } from '@/db/schema';
 import { DatabaseError } from '@/errors/api-error';
-import { and, eq, gt } from 'drizzle-orm';
+import { and, asc, eq, gt } from 'drizzle-orm';
 
 export class ScheduleService {
   async getUpcomingSchedulesByUserId(
@@ -17,7 +17,8 @@ export class ScheduleService {
             eq(userSchedule.userId, email),
             gt(userSchedule.start_time, new Date())
           )
-        );
+        )
+        .orderBy(asc(userSchedule.status), asc(userSchedule.start_time));
     } catch (error) {
       console.error('Database error when fetching schedules:', error);
       throw new DatabaseError('Failed to retrieve schedules');
